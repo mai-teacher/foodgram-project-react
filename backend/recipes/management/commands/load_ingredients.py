4c45da6cd196fa99a75aa1224d28da4f3e3e9d8d
Александр Макеев <mai-teacher@yandex.ru>
2023-09-01 10:30:55 +0300

    Actions 3.8

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -9,16 +9,8 @@ class Command(BaseCommand):
     help = 'load ingredients from csv'
 
     def handle(self, *args: Any, **options: Any):
-        with open('../data/ingredients.csv', encoding='utf-8') as csv_file:
+        with open('/data/ingredients.csv', encoding='utf-8') as csv_file:
             csv_reader = csv.DictReader(csv_file, delimiter=',')
-        #     lines = 0
-        #     for row in csv_reader:
-        #         name = row['name']
-        #         unit = row['measurement_unit']
-        #         ingredient = Ingredient(name=name, measurement_unit=unit)
-        #         ingredient.save()
-        #         lines += 1
-        # print(f'   imported: {lines} lines')
             Ingredient.objects.bulk_create(
                 Ingredient(**data) for data in csv_reader)
             self.stdout.write(self.style.SUCCESS(
