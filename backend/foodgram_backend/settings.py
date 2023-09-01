706260035d3ff2a369ff91745c085b45d3141e3a
Александр Макеев <mai-teacher@yandex.ru>
2023-09-01 13:00:41 +0300

    Action 3.9

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -147,10 +147,14 @@ USE_TZ = True
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = '/media'
 # MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATIC_ROOT = BASE_DIR / 'collected_static'
 # STATIC_ROOT = BASE_DIR / 'backend_static/static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
