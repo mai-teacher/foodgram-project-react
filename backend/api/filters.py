392ce2df8919d56e21a6848e4d0f015aa6c4e8b2
Александр Макеев <mai-teacher@yandex.ru>
2023-09-02 14:40:41 +0300

    Change favorite/shopping_cart 8

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -20,13 +20,27 @@ class RecipeFilter(FilterSet):
         field_name='author__id', label='Автор'
     )
     tags = filters.AllValuesMultipleFilter(field_name="tags__slug")
-    is_in_shopping_cart = filters.BooleanFilter(
-        widget=BooleanWidget(), label='В списке покупок'
-    )
-    is_favorited = filters.BooleanFilter(
-        widget=BooleanWidget(), label='В избранном'
-    )
+    # is_in_shopping_cart = filters.BooleanFilter(
+    #     widget=BooleanWidget(), label='В списке покупок'
+    # )
+    # is_favorited = filters.BooleanFilter(
+    #     widget=BooleanWidget(), label='В избранном'
+    # )
+
+    is_favorited = filters.NumberFilter(method='get_is_favorited')
+    is_in_shopping_cart = filters.NumberFilter(
+        method='get_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
         fields = ('author', 'tags', 'is_in_shopping_cart', 'is_favorited')
+
+    def get_is_favorited(self, queryset, name, value):
+        if self.request.user.is_authenticated and value:
+            return queryset.filter(favorites__user=self.request.user)
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        if self.request.user.is_authenticated and value:
+            return queryset.filter(shopping_cart__user=self.request.user)
+        return queryset
