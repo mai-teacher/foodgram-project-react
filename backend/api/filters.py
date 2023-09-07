2acfcc733b46f4fa250fad7437660fc9dd606a1f
Александр Макеев <mai-teacher@yandex.ru>
2023-09-07 17:58:42 +0300

    Change Filter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -33,5 +33,5 @@ class RecipeFilter(FilterSet):
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         if self.request.user.is_authenticated and value:
-            return queryset.filter(shopping_cart__user=self.request.user)
+            return queryset.filter(shoppingcarts__user=self.request.user)
         return queryset
